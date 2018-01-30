f42439c1147271f97bf6983a0c0295f56c5416bb
Abai <abai2010@bk.ru>
2018-01-30 17:27:50 +0600

    finished

---
+++ Exam/Employee.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        private int id;
+        private string fullName;
+        private string position;
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set
+            {
+                fullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+        public string Position
+        {
+            get
+            {
+                return position;
+            }
+            set
+            {
+                position = value;
+                OnPropertyChanged("Position");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Exam/Employees.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam
+{
+    /// <summary>
+    /// Interaction logic for Employees.xaml
+    /// </summary>
+    public partial class Employees : Page
+    {
+        private ObservableCollection<Employee> employees;
+        public Employees()
+        {
+            InitializeComponent();
+
+            employees = new ObservableCollection<Employee>()
+            {
+                new Employee
+                {
+                    Id = 1,
+                    FullName = "Ким Олег Павлович",
+                    Position = "директор"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    FullName = "Тен Павел Юрьевич",
+                    Position = "разработчик"
+                }
+            };
+
+            dataContainer.ItemsSource = employees;
+        }
+
+        private void ButtonInfoClick(object sender, RoutedEventArgs e)
+        {
+            //сериализация userList в файл (json)
+            //List<Employee> userList = File.Exists(@"C:\name_of_folder\data.json") ? JsonConvert.DeserializeObject<ObservableCollection<Employee>>(File.ReadAllText(@"C:\name_of_folder\data.json")) : new List<User>();
+
+            if (!(Directory.Exists(@"C:\name_of_folder")))
+            {
+                Directory.CreateDirectory(@"C:\name_of_folder");
+            }
+
+            string json = JsonConvert.SerializeObject(employees);
+
+            using (StreamWriter writer = new StreamWriter(@"C:\name_of_folder\data.json"))
+            {
+                writer.WriteLine(json);
+            }
+
+            //переход к Info странице
+            Info infoPage = new Info();
+            this.NavigationService.Navigate(infoPage);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string satr = "ert";
+        }
+    }
+}
